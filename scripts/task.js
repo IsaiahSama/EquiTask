de5b057958ae50a1145cc0087fc5e31fc3caaafa
Isaiah-Sama <zeldaplayergl@gmail.com>
2024-01-07 18:25:23 -0400

    Now has a method to return HTML for the form

--- scripts/task.js
+++ scripts/task.js
@@ -1,8 +1,8 @@
 class Task {
-  constructor(name, desc) {
+  constructor(name, desc, day) {
     this.name = name;
     this.desc = desc;
-    this.day = null;
+    this.day = day;
     this.completed = false;
   }
 
@@ -17,12 +17,42 @@ class Task {
     delete this;
   };
 
-  updateTask = function (name, desc) {
+  updateTask = function (name, desc, day) {
     this.name = name == null ? this.name : name;
     this.desc = desc == null ? this.desc : desc;
+    this.day = day == null ? this.day : day;
   };
 
-  getAsHTML = function () {
+  getAsFormHTML = function () {
     // Will return some HTML for rendering
+    /*  <div class="check-container">
+            <label for="task1" class="checkbox">
+                <input type="checkbox" name="task1" id="task1" />
+                Some Tuesday Task
+            </label>
+        </div>; 
+    */
+
+    // Creating the container
+    const container = document.createElement("div");
+    container.className = "check-container";
+
+    // Creating the label
+    const label = document.createElement("label");
+    label.setAttribute("for", this.name);
+    label.className = "checkbox";
+
+    // Creating the input box
+    const inp = document.createElement("input");
+    inp.setAttribute("type", "checkbox");
+    inp.setAttribute("name", this.name);
+    inp.setAttribute("id", this.name);
+
+    // Putting it all together
+    label.appendChild(inp);
+    label.innerText = this.name;
+    container.appendChild(label);
+
+    return container;
   };
 }
