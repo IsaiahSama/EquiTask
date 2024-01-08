4b1c9d38fe06549a57feef5d7d1a5b7c333d972b
Isaiah-Sama <zeldaplayergl@gmail.com>
2024-01-08 08:19:33 -0400

    Can now get the checked elements

--- scripts/dist.js
+++ scripts/dist.js
@@ -1,11 +1,15 @@
+let form = document.getElementById("distForm");
+let ids = [];
+
 function loadSelectableTasks() {
   const cont = document.getElementById("taskContainer");
   const allTasks = getAllItemsFromStorage();
-  let elements = [];
 
+  let elements = [];
   for (let key of Object.keys(allTasks)) {
     let t = new Task(key, allTasks[key], null);
     elements.push(t.getAsFormHTML());
+    ids.push(key);
   }
 
   if (elements.length == 0) {
@@ -20,3 +24,17 @@ function loadSelectableTasks() {
 }
 
 loadSelectableTasks();
+
+function getChecked(ids) {
+  const elements = [];
+  for (let id of ids) {
+    let element = document.getElementById(id);
+    if (element && element.checked) elements.push(element);
+  }
+  return elements;
+}
+
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  let tasks = getChecked(ids);
+});
