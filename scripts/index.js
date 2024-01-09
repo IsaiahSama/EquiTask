b594d92929771b35ff460038d646922947a8f192
Isaiah-Sama <zeldaplayergl@gmail.com>
2024-01-08 20:54:11 -0400

    Completed base rendering

--- scripts/index.js
+++ scripts/index.js
@@ -6,19 +6,23 @@ const renderTable = () => {
   }
 
   let row = 0;
+  let tbody = document.getElementById("tableBody");
   while (row < longest) {
-    tasks_to_render = [];
+    let tr = document.createElement("tr");
 
     for (let day of days) {
       if (day in allDays && allDays[day].length > 0) {
         let task_name = allDays[day].pop();
         let task_data = getTaskFromStorage(task_name);
         let task = new Task(task_name, task_data[task_name], day);
-        tasks_to_render.push(task.getTableHTML());
+        tr.appendChild(task.getTableHTML());
+      } else {
+        tr.appendChild(document.createElement("td"));
       }
     }
+    tbody.appendChild(tr);
     row += 1;
   }
 };
 
-renderTable();
+setTimeout(renderTable, 1000);
