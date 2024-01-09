b2eee6ce005e69ce9b256a71493cf345c7b3ed81
Isaiah-Sama <zeldaplayergl@gmail.com>
2024-01-08 20:08:56 -0400

    Added a shuffil method

--- scripts/utils.js
+++ scripts/utils.js
@@ -74,3 +74,10 @@ const createCheckboxElement = (labelValue) => {
 
   return container;
 };
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
